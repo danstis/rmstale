8877d1a22201f6b8b3ccb5131ecd61fc7713e105
Dan Anstis <dan@bsod.co.nz>
2018-02-08 16:07:15 +1000

    Update build script

--- build.ps1
+++ build.ps1
@@ -18,19 +18,3 @@ go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\linux\rms
 ## Mac x86
 $ENV:GOOS = "darwin"; $env:GOARCH = "386"
 go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\darwin\rmstale rmstale.go
-
-$content = [System.IO.File]::ReadAllText("rmstale.nuspec").Replace("__REPLACE_VERSION__",$VersionInfo.SemVer)
-[System.IO.File]::WriteAllText("rmstale.nuspec", $content)
-
-## Removed builds
-## Win AMD64
-# $ENV:GOOS = "windows"; $env:GOARCH = "amd64"
-# go build -race -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)"-o .\bin\rmstale-win_amd64.exe rmstale.go
-
-## Linux AMD64
-# $ENV:GOOS = "linux"; $env:GOARCH = "amd64"
-# go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\rmstale-linux_amd64 rmstale.go
-
-## Mac AMD64
-# $ENV:GOOS = "darwin"; $env:GOARCH = "amd64"
-# go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\rmstale-mac_amd64 rmstale.go
