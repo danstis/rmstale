fa5f21ce15b433a4e54a53d436f3ec7084fcbd03
Dan Anstis <dan.anstis@bdo.global>
2021-08-07 15:33:05 +0100

    chore: update build script

--- build.ps1
+++ build.ps1
@@ -1,22 +1,22 @@
 # Write-Host "Getting GitVersion"
 # choco install gitversion.portable -pre -y
 
-Write-Host "Generating version info using GitVersion"
-$VersionInfo = gitversion | convertfrom-json
+Write-Host 'Generating version info using GitVersion'
+$VersionInfo = gitversion | ConvertFrom-Json
 
 Write-Host ("Version detected as '{0}'" -f $VersionInfo.SemVer)
 
 # Build binaries
 ## Win x86
-$ENV:GOOS = "windows"; $env:GOARCH = "386"
+$ENV:GOOS = 'windows'; $env:GOARCH = '386'
 go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\windows\rmstale.exe rmstale.go
 
 ## Linux x86
-$ENV:GOOS = "linux"; $env:GOARCH = "386"
+$ENV:GOOS = 'linux'; $env:GOARCH = '386'
 go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\linux\rmstale rmstale.go
 
 ## Mac x86
-$ENV:GOOS = "darwin"; $env:GOARCH = "386"
+$ENV:GOOS = 'darwin'; $env:GOARCH = 'amd64'
 go build -ldflags "-X main.AppVersion=$($VersionInfo.SemVer)" -o .\bin\darwin\rmstale rmstale.go
 
-$ENV:GOOS = "windows"; $env:GOARCH = "amd64"
+$ENV:GOOS = 'windows'; $env:GOARCH = 'amd64'
